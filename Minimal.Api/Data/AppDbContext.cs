9404eff2a3ea970df5d274dd6a3bdb16d11a4434
Sara Graziele Veronez <sgveronez@paschoalotto.com.br>
2022-02-28 13:10:39 -0300

    dockerfile e docker compose

--- Minimal.Api/Data/AppDbContext.cs
+++ Minimal.Api/Data/AppDbContext.cs
@@ -7,6 +7,7 @@ namespace Minimal.Api.Data
     {
         public DbSet<User> Users { get; set; }
 
+        //alterar pra localhost para executar migraton no visual studio
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             => optionsBuilder.UseSqlServer("Server=localhost,1433;Database=MNIMAL_API;User=sa;Password=Secret@1234");
     }
