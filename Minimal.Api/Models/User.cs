9d27b7298fb082dde42843658c4451fe00d9eb69
Sara Graziele Veronez <sgveronez@paschoalotto.com.br>
2022-02-28 14:50:35 -0300

    sintaxe record

--- Minimal.Api/Models/User.cs
+++ Minimal.Api/Models/User.cs
@@ -1,19 +1,4 @@
 ﻿namespace Minimal.Api.Models
 {
-    public record User
-    {
-        public User()
-        {
-
-        }
-        public User(Guid id, string nome, DateTime dataCriacao)
-        {
-            Id = id;    
-            Nome = nome;
-            DaTaCriacao = dataCriacao;
-        }
-        public Guid Id { get; set; }
-        public string Nome { get; set; }
-        public DateTime DaTaCriacao { get; set; }
-    }
+    public record User(Guid Id, string Nome, DateTime DaTaCriacao);
 }
